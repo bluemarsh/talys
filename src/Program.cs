933a3dbefcce92d97a0b422006c3365a46d56c81
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2019-04-11 16:58:17 -0700

    Project boilerplate

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GiantBombDataTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
