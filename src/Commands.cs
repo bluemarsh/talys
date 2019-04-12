85d9382aa274e90c984942cd647a4df6150f37cd
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2019-04-12 10:46:24 -0700

    Basic command line parsing

---
+++ src/Commands.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GiantBombDataTool
+{
+    [Verb("clone")]
+    class CloneCommand
+    {
+        public int Execute()
+        {
+            Console.WriteLine("Execute Clone");
+            return 0;
+        }
+    }
+
+    [Verb("fetch")]
+    class FetchCommand
+    {
+        public int Execute()
+        {
+            Console.WriteLine("Execute Fetch");
+            return 0;
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using CommandLine;
 
 namespace GiantBombDataTool
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Trace.Listeners.Add(new ConsoleTraceListener(useErrorStream: true));
+
+            int result = Parser.Default.ParseArguments<CloneCommand, FetchCommand>(args).MapResult(
+                (CloneCommand c) => c.Execute(),
+                (FetchCommand c) => c.Execute(),
+                errors => IsHelpOrVersionRequest(errors) ? 0 : 1);
+
+            if (Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.Read();
+            }
+
+            return result;
+        }
+
+        private static bool IsHelpOrVersionRequest(IEnumerable<Error> errors)
+        {
+            return errors.Any(e =>
+                e.Tag == ErrorType.HelpRequestedError ||
+                e.Tag == ErrorType.HelpVerbRequestedError ||
+                e.Tag == ErrorType.VersionRequestedError);
+        }
+
+        // Not present in .NET Core 2.1 (System.Diagnostics.ConsoleTraceListener)
+        class ConsoleTraceListener : TextWriterTraceListener
+        {
+            public ConsoleTraceListener(bool useErrorStream = false)
+                : base(useErrorStream ? Console.Error : Console.Out)
+            {
+            }
+
+            public override void Close()
+            {
+                // No resources to clean up.
+            }
         }
     }
 }
