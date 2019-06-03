87157a33695ad3575ba071e2d0655e773fba8be0
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2019-06-03 10:35:44 -0700

    Missed new file from last commit

---
+++ src/CollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Talys
+{
+    internal static class CollectionExtensions
+    {
+        public static void Add<T>(this HashSet<T> set, IEnumerable<T> items)
+        {
+            set.UnionWith(items);
+        }
+
+        public static void ReplaceWith<T>(this HashSet<T> set, IEnumerable<T> items)
+        {
+            set.Clear();
+            set.UnionWith(items);
+        }
+    }
+}
